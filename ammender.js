bd0ad3669e866996cd7c457bf034fd84cb14a382
Mila Frerichs <mila.frerichs@gmail.com>
2014-04-25 10:57:26 +0200

    template should be added with an empty line
    What does this PR do?
    ---------
    preserves the commit message

--- ammender.js
+++ ammender.js
@@ -2,6 +2,6 @@ var exec = require('child_process').exec;
 
 module.exports = function (message, fn) {
   exec("git log -1 --pretty=%B", function(err, stdout, stderr){
-    exec("echo \""+stdout+"\n"+message+"\" | git commit --amend --file -", fn);
+    exec("echo \""+stdout+"\n\n"+message+"\" | git commit --amend --file -", fn);
   });
 }
