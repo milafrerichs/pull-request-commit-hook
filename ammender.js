7dc94857a720c4b18d6e1f01019a994dae03363b
Mila Frerichs <mila.frerichs@gmail.com>
2014-04-25 10:38:48 +0200

    preserve commit message
    PR with Template
    What does this PR do?
    ---------
    Preserves the commit message

--- ammender.js
+++ ammender.js
@@ -1,5 +1,8 @@
 var exec = require('child_process').exec;
 
 module.exports = function (message, fn) {
-  exec("echo \""+message+"\" | git commit --amend --file -", fn);
+  exec("git log -1 --pretty=%B", function(err, stdout, stderr){
+    console.log(stdout)
+    exec("echo \""+stdout+"\n"+message+"\" | git commit --amend --file -", fn);
+  });
 }
